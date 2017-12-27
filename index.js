2833aeb72cdfa7e62cf74c715499073ed313bb4e
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-27 01:59:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   var lowerCase = string.toLowerCase
+  var upperCase = strring.toUpperCase
   if(string = lowerCase){
-    return "I can\'t hear you"
-  }else if(upperCase === true){
+    return "I can\'t hear you!"
+  }else if(string = upperCase){
     return "YES INDEED!"
   }else if(string === "I love you, Grandma."){
     return "I love you, too."
