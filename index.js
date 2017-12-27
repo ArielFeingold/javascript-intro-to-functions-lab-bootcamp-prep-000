98dbb47c367f3b50950425bbf15043bbdd04400e
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-27 01:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string) {
 };
 
 function sayHiToGrandma(string){
-  var upperCase = string.toUpperCase()
-  var lowerCase = string.toLowerCase()
+
   if(lowerCase === true){
     return "I can\'t hear you"
   }else if(upperCase === true){
