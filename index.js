a4e9c61ade7ffe8a9330b0017ec8bf694f30bc84
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-27 02:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 };
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   var lowerCase = string.toLowerCase
   var upperCase = strring.toUpperCase
   if(string = lowerCase){
