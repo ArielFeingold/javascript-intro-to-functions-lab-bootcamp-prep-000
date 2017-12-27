544e82ec66fb577c4e42faef6f6f734d7427c1d3
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-27 01:25:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,16 @@ function logWhisper(string) {
 };
 
 function sayHiToGrandma(string) {
-  var lowerCase = string.toLowerCase();
-  if(lowerCase = true){
+  var upperCase = string.toUpperCase();
+  if(upperCase = true){
     return "I can\'t hear you!"
+  }else if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }else{
+    
+  }
+  }
+    
   };
 };
 
